c3e1104d70a16d2858c03312b015dbba5b0cd2f3
zhaoyang0501@126.com <zhaoyang0501@126.com>
2016-05-16 19:40:41 +0800

    代码精简

--- src/main/java/com/pzy/service/GradeService.java
+++ src/main/java/com/pzy/service/GradeService.java
@@ -1,7 +1,6 @@
 
 package com.pzy.service;
 
-import java.sql.Date;
 import java.util.List;
 
 import javax.persistence.criteria.CriteriaBuilder;
@@ -9,22 +8,17 @@ import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 
-import org.apache.commons.lang3.time.DateUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.jpa.domain.Specification;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.pzy.entity.Grade;
 import com.pzy.repository.GradeRepository;
-import com.pzy.repository.ReportRepository;
 /***
  * 
  * @author qq:263608237
@@ -35,9 +29,6 @@ import com.pzy.repository.ReportRepository;
 public class GradeService {
      @Autowired
      private GradeRepository gradeRepository;
-     @Autowired
-     private ReportRepository reportRepository;
-
  	public List<Grade> findTop3() {
  		return gradeRepository.findAll(
  				new PageRequest(0, 15, new Sort(Direction.DESC, "createDate")))
@@ -45,9 +36,6 @@ public class GradeService {
  	}
      public List<Grade> findAll() {
     	 List<Grade> grades=(List<Grade> ) gradeRepository.findAll(new Sort(Direction.DESC, "id"));
-    	 for(Grade grade: grades){
-    		 grade.setUsernum(reportRepository.findByGrade(grade).size());
-    	 }
     	 return grades;
      }
      public Page<Grade> findAll(final int pageNumber, final int pageSize,final String name){
@@ -90,20 +78,4 @@ public class GradeService {
 		}
 		
 		
-		public void AutoEndGrade(){
-			System.out.println("fuckyou!----");
-			 List<Grade> grades=(List<Grade> ) gradeRepository.findAll(new Sort(Direction.DESC, "id"));
-	    	 for(Grade grade: grades){
-	    		 grade.setUsernum(reportRepository.findByGrade(grade).size());
-	    	 }
-	    	 
-	    	 for(Grade grade: grades){
-	    		if(grade.getUsernum()>=30||grade.getEnd().before(new Date(System.currentTimeMillis()))){
-	    			grade.setState("报名结束");
-	    			grade.setCreateDate(new Date(System.currentTimeMillis()));
-	    			this.save(grade);
-	    		}
-	    	 }
-	    	 System.out.println("fuckyouend!----");
-		}
 }
--- src/main/webapp/WEB-INF/views/validateLogin.jsp
+++
@@ -1,5 +0,0 @@
-<%@ page language="java" contentType="text/html; charset=utf-8" pageEncoding="utf-8"%>
-<%
-if(request.getSession().getAttribute("user")==null)
-response.sendRedirect("login");
-%>  
